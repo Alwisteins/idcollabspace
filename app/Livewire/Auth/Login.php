b5ca19b0e1260538b37db59eb855a1792ed6ae90
Alwisteins <alwi.wahyu.w@gmail.com>
2025-10-24 21:24:47 +0700

    feat(login): finished login page

--- app/Livewire/Auth/Login.php
+++ app/Livewire/Auth/Login.php
@@ -2,6 +2,7 @@
 
 namespace App\Livewire\Auth;
 
+use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
 
 class Login extends Component
@@ -9,5 +10,20 @@ class Login extends Component
     public function render()
     {
         return view('livewire.auth.login');
+    }
+
+    public $email;
+    public $password;
+
+    public function login()
+    {
+        Auth::attempt([
+            'email' => $this->email,
+            'password' => $this->password
+        ]);
+
+        if (Auth::check()) {
+            return Auth::user()->is_onboarded ? redirect()->route('home') : redirect()->route('onboarding');
+        }
     }
 }
--- resources/views/livewire/auth/login.blade.php
+++ resources/views/livewire/auth/login.blade.php
@@ -22,7 +22,7 @@
                         <input type="password" name="password" id="password" placeholder="••••••••"
                             class="form-input @error('password') form-input-error @enderror" wire:model="password">
                     </div>
-                    <div class="flex items-center justify-between">
+                    {{-- <div class="flex items-center justify-between">
                         <div class="flex items-start">
                             <div class="flex items-center h-5">
                                 <input id="remember" aria-describedby="remember" type="checkbox"
@@ -36,7 +36,7 @@
                         <a href="#"
                             class="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500">Forgot
                             password?</a>
-                    </div>
+                    </div> --}}
                     <button type="submit"
                         class="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Login</button>
                     <p class="text-sm text-gray-500 dark:text-gray-400">
