2dac820ba0ee8f0a1a1565e03e7685fc0459b346
Alwisteins <alwi.wahyu.w@gmail.com>
2025-11-11 17:06:15 +0700

    feat(talent): add search features

--- app/Livewire/Talents/Talent.php
+++ app/Livewire/Talents/Talent.php
@@ -4,13 +4,38 @@ namespace App\Livewire\Talents;
 
 use App\Models\User;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class Talent extends Component
 {
+    use WithPagination;
+
+    public $search = '';
+
+    public $queryString = [
+        'search' => ['except' => '']
+    ];
+
+    public function updatingSearch()
+    {
+        $this->resetPage();
+    }
+
     public function render()
     {
+        $query = User::where('globals_role', 'user')->where('is_onboarded', '1')->with('roles')->latest();
+
+        if ($this->search) {
+            $search = '%' . $this->search . '%';
+            $query->where('name', 'like', $search)
+                ->orWhere('location', 'like', $search)
+                ->orWhereHas('roles', function ($q) use ($search) {
+                    $q->where('name', 'like', $search);
+                });
+        }
+
         return view('livewire.talents.talent', [
-            'talents' => User::where('globals_role', 'user')->where('is_onboarded', '1')->with('roles')->latest()->paginate(20)
+            'talents' => $query->paginate(20)
         ]);
     }
 }
--- resources/views/livewire/talents/talent.blade.php
+++ resources/views/livewire/talents/talent.blade.php
@@ -1,5 +1,19 @@
-<div>
-    <div class="grid md:grid-cols-2 lg:grid-cols-3 gap-4 p-6">
+<div class="p-6">
+    <div class="mb-6">
+        <div class="relative">
+            <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+                <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                    fill="none" viewBox="0 0 20 20">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                        d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
+                </svg>
+            </div>
+            <input type="search" id="default-search" wire:model.debounce.live.500ms="search"
+                class="block w-96 h-full ps-10 px-5 py-3 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder="Cari Nama, Lokasi atau Role..." required />
+        </div>
+    </div>
+    <div class="grid md:grid-cols-2 lg:grid-cols-3 gap-4">
         @foreach ($talents as $talent)
             <div wire:navigate href="{{ route('talents.show', $talent['id']) }}"
                 class="bg-white flex flex-col rounded-2xl p-6 hover:shadow-xl transition-all border border-gray-200 group cursor-pointer hover:-translate-y-2">
